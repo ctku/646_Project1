f5b17b5f5ba016d5e5afb88fbcfdeab84d012777
Chung-Ta Ku <ctku@opensub01.umiacs.umd.edu>
2013-09-16 22:18:33 -0400

    init commit

---
+++ pipeline.c
@@ -0,0 +1,32 @@
+
+/*
+ * 
+ * pipeline.c
+ * 
+ * Donald Yeung
+ */
+
+
+#include <stdlib.h>
+#include "fu.h"
+#include "pipeline.h"
+
+
+void
+writeback(state_t *state, int *num_insn) {
+}
+
+
+void
+execute(state_t *state) {
+}
+
+
+int
+decode(state_t *state) {
+}
+
+
+void
+fetch(state_t *state) {
+}
